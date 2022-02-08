6a060804af6b9dc7d679f5a8cbf59452839dafc4
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2022-02-08 22:26:48 +0300

    Add portfolio body

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    screenshot=CloudinaryField('image',default='Image')
+    topics=models.CharField(max_length=200,null=True)
+    description = models.TextField(max_length=300,null=True)
+    live_link = models.CharField(max_length=50,null=True)
+
+      
+    def __str__(self):
+        return self.name
+    
+    def save_project(self):
+        self.save()
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Project
+
 
 # Create your views here.
+def index(request):
+    project=Project.objects.all()
+
+    return render(request, 'main/index.html',{'projects':project})
